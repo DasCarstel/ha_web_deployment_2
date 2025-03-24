3b703c9cc49d5c92cfc6fb9458e4ff35c56f0e13
Carsten Mueller <carstenmueller2002@gmail.com>
2025-03-24 15:11:53 +0100

    initial commit

---
+++ src/main/java/com/example/ha_web_deployment_2/HaWebDeployment2Application.java
@@ -0,0 +1,13 @@
+package com.example.ha_web_deployment_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaWebDeployment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaWebDeployment2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ha_web_deployment_2/HaWebDeployment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ha_web_deployment_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaWebDeployment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
