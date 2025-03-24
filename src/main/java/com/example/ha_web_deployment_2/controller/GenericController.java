3e05673acba5ef498a7c63705cc55692901ae66b
Carsten Mueller <carstenmueller2002@gmail.com>
2025-03-24 15:30:54 +0100

    added generic controller

---
+++ src/main/java/com/example/ha_web_deployment_2/controller/GenericController.java
@@ -0,0 +1,44 @@
+package com.example.ha_web_deployment_2.controller;
+
+import com.example.ha_web_deployment_2.models.GenericEntity;
+import com.example.ha_web_deployment_2.models.GenericRepository;
+import com.example.ha_web_deployment_2.services.GenericService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public abstract class GenericController<T extends GenericEntity<T>> {
+
+    private final GenericService<T> service;
+
+    public GenericController(GenericRepository<T> repository) {
+        this.service = new GenericService<T>(repository) {};
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Page<T>> getPage(Pageable pageable){
+        return ResponseEntity.ok(service.getPage(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<T> getOne(@PathVariable Long id){
+        return ResponseEntity.ok(service.get(id));
+    }
+
+    @PutMapping("")
+    public ResponseEntity<T> update(@RequestBody T updated){
+        return ResponseEntity.ok(service.update(updated));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<T> create(@RequestBody T created){
+        return ResponseEntity.ok(service.create(created));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.ok("Ok");
+    }
+}
